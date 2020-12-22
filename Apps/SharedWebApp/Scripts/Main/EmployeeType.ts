5f9f3b11e890222b6dd1e674ae015333322840be
Jason Benfield <jbenfield108@gmail.com>
2020-12-22 18:16:15 -0500

    Add numeric value

---
+++ Apps/SharedWebApp/Scripts/Main/EmployeeType.ts
@@ -0,0 +1,24 @@
+﻿import { NumericValue } from '../Shared/NumericValue';
+import { NumericValues } from '../Shared/NumericValues';
+
+export class EmployeeTypes extends NumericValues<EmployeeType> {
+    constructor(
+        public readonly none: EmployeeType,
+        public readonly temp: EmployeeType,
+        public readonly permanent: EmployeeType
+    ) {
+        super([none, temp, permanent]);
+    }
+}
+
+export class EmployeeType extends NumericValue {
+    public static readonly values = new EmployeeTypes(
+        new EmployeeType(0, 'None'),
+        new EmployeeType(1, 'Temp'),
+        new EmployeeType(2, 'Permanent')
+    );
+
+    private constructor(Value: number, DisplayText: string) {
+        super(Value, DisplayText);
+    }
+}
---
+++ Apps/SharedWebApp/Scripts/Tests/NumericValueTest.ts
@@ -0,0 +1,29 @@
+﻿import 'mocha';
+import { expect } from 'chai';
+import { EmployeeType } from '../Main/EmployeeType';
+
+describe('Numeric Values', () => {
+    it('Numeric value from numeric value', function () {
+        let employeeType = EmployeeType.values.value(1);
+        expect(employeeType.DisplayText).to.equal('Temp');
+    });
+    it('Numeric value from display text', function () {
+        let employeeType = EmployeeType.values.value('Permanent');
+        expect(employeeType.DisplayText).to.equal('Permanent');
+    });
+    it('Equal by numeric value', function () {
+        expect(EmployeeType.values.permanent.equals(2)).to.be.true;
+    });
+    it('Not Equal by numeric value', function () {
+        expect(EmployeeType.values.temp.equals(2)).to.be.false;
+    });
+    it('Equal by display text', function () {
+        expect(EmployeeType.values.temp.equals('Temp')).to.be.true;
+    });
+    it('Equal by display text is case insensitive', function () {
+        expect(EmployeeType.values.temp.equals('tEmP')).to.be.true;
+    });
+    it('Not Equal by display text', function () {
+        expect(EmployeeType.values.temp.equals('Permanent')).to.be.false;
+    });
+});
