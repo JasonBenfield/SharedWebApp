792b9923036c2c9f0e0f9befce463e17ebc97f2e
Jason Benfield <jbenfield108@gmail.com>
2021-01-09 10:44:48 -0500

    Tag helper for knockout component

---
+++ Lib/XTI_WebApp.TagHelpers/XtiFieldTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using XTI_Forms;
+
+namespace XTI_WebApp.TagHelpers
+{
+    [HtmlTargetElement("xti-field")]
+    public class XtiFieldTagHelper : TagHelper
+    {
+        public XtiFieldTagHelper()
+        {
+        }
+
+        [HtmlAttributeName("field")]
+        public IField Field { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "";
+            output.PreContent.AppendHtml($"<!-- ko with: {Field.Name} -->");
+            output.PreContent.AppendHtml("<!-- ko component: { name: componentName, params: $data } --><!-- /ko -->");
+            output.PostContent.AppendHtml("<!-- /ko -->");
+        }
+    }
+}
