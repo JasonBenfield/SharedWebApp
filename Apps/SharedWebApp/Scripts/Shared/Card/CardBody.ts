3853534440cfb074195178c85e47d1afaa4a6b26
Jason Benfield <jbenfield108@gmail.com>
2021-12-21 07:34:33 -0500

    deleted

--- Apps/SharedWebApp/Scripts/Shared/Card/CardBody.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Block } from "../Html/Block";
-import { BlockViewModel } from "../Html/BlockViewModel";
-
-export class CardBody extends Block {
-    constructor(vm: BlockViewModel = new BlockViewModel()) {
-        super(vm);
-        this.vm.css('card-body');
-    }
-}
