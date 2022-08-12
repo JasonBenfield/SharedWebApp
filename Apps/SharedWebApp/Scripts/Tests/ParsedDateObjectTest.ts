785deb64ce56afb4f7c8dd7b698731ef77e610cb
Jason Benfield <jbenfield108@gmail.com>
2022-08-12 15:46:53 -0400

    fix ParsedDateObject

---
+++ Apps/SharedWebApp/Scripts/Tests/ParsedDateObjectTest.ts
@@ -0,0 +1,12 @@
+﻿import { expect } from 'chai';
+import 'mocha';
+import { ParsedDateObject } from '../Lib/Api/ParsedDateObject';
+
+describe('Parsed Date Object', () => {
+    it('Date Strings', function () {
+        const str = '{"StartDate": "2001-01-01T00:00:00+00:00"}';
+        const parsed = new ParsedDateObject(JSON.parse(str)).value;
+        const startDate: Date = parsed.StartDate;
+        expect(startDate.getTime()).to.equal(Date.UTC(2001, 0, 1));
+    });
+});
