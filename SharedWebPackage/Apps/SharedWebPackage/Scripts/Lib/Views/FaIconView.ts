0f5711aabba99cd506a10e200e371eeebf3a1cb0
jbenfield108@gmail.com <jbenfield108@gmail.com>
2025-01-17 07:32:53 -0500

    Flip FaIcon veritically and horizontally

--- SharedWebPackage/Apps/SharedWebPackage/Scripts/Lib/Views/FaIconView.ts
+++ SharedWebPackage/Apps/SharedWebPackage/Scripts/Lib/Views/FaIconView.ts
@@ -1,8 +1,8 @@
-﻿import { ContextualClass } from '../ContextualClass';
-import { BasicComponentView } from './BasicComponentView';
-import { IconView } from './IconView';
+﻿import { ContextualClass } from "../ContextualClass";
+import { BasicComponentView } from "./BasicComponentView";
+import { IconView } from "./IconView";
 
-export type FaIconAnimation = 'spin' | 'pulse';
+export type FaIconAnimation = "spin" | "pulse";
 
 export class FaIconView extends IconView {
     constructor(container: BasicComponentView) {
@@ -10,73 +10,85 @@ export class FaIconView extends IconView {
     }
 
     setColor(color: ContextualClass = ContextualClass.default) {
-        this.setCss('text-color', color.append('text'));
+        this.setCss("text-color", color.append("text"));
     }
 
     regularStyle(name: string) {
-        this.setPrefix('fa-regular');
+        this.setPrefix("fa-regular");
         if (name) {
             this.setIconName(name);
         }
     }
 
     solidStyle(name: string) {
-        this.setPrefix('fa-solid');
+        this.setPrefix("fa-solid");
         this.setIconName(name);
     }
 
     private setPrefix(prefix: string) {
-        this.setCss('prefix', prefix);
+        this.setCss("prefix", prefix);
     }
 
     private setIconName(name: string) {
         name = this.normalizeName(name);
-        this.setCss('icon-name', name);
+        this.setCss("icon-name", name);
     }
 
     private normalizeName(name: string) {
-        if (name && name.indexOf('fa-') !== 0) {
+        if (name && name.indexOf("fa-") !== 0) {
             name = `fa-${name}`;
         }
         return name;
     }
 
     makeFixedWidth() {
-        this.addCssName('fa-fw');
+        this.addCssName("fa-fw");
     }
 
-    resize(size: '' | 'xs' | 'sm' | 'lg' | '1X' | '2X' | '3X' | '4X' | '5X' | '6X' | '7X' | '8X' | '9X' | '10X') {
+    resize(size: "" | "xs" | "sm" | "lg" | "1X" | "2X" | "3X" | "4X" | "5X" | "6X" | "7X" | "8X" | "9X" | "10X") {
         const sizeCss = size ? `fa-${size}` : size;
-        this.setCss('icon-size', sizeCss);
+        this.setCss("icon-size", sizeCss);
     }
 
     makeBordered() {
-        this.addCssName('fa-bordered');
+        this.addCssName("fa-bordered");
+    }
+
+    flipHorizontally() {
+        this.addCssName("fa-flip-horizontal");
+    }
+
+    flipVertically() {
+        this.addCssName("fa-flip-vertical");
+    }
+
+    flipBoth() {
+        this.addCssName("fa-flip-both");
     }
 
     rotate(howMuch: 0 | 90 | 180 | 270) {
-        let rotationCss = howMuch ? `fa-rotate-${howMuch}` : '';
-        this.setCss('icon-rotation', rotationCss);
+        let rotationCss = howMuch ? `fa-rotate-${howMuch}` : "";
+        this.setCss("icon-rotation", rotationCss);
     }
 
     pullLeft() {
-        this.pull('fa-pull-left');
+        this.pull("fa-pull-left");
     }
 
     pullRight() {
-        this.pull('fa-pull-right');
+        this.pull("fa-pull-right");
     }
 
     private pull(pulled: string) {
-        this.setCss('icon-pulled', pulled);
+        this.setCss("icon-pulled", pulled);
     }
 
     startAnimation(animation: FaIconAnimation) {
         let css = animation ? `fa-${animation}` : animation;
-        this.setCss('icon-animation', css);
+        this.setCss("icon-animation", css);
     }
 
     stopAnimation() {
-        this.setCss('icon-animation', '');
+        this.setCss("icon-animation", "");
     }
 }
