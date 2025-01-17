42629da0bfd0eea1cb63b5c8ab53f9c5a3b6377f
Jason Benfield <jbenfield108@gmail.com>
2025-01-17 07:46:58 -0500

    add options to text css

--- SharedWebPackage/Apps/SharedWebPackage/Scripts/Lib/TextCss.ts
+++ SharedWebPackage/Apps/SharedWebPackage/Scripts/Lib/TextCss.ts
@@ -4,25 +4,25 @@ import { CssClass } from "./CssClass";
 export class TextCss implements ICssBuilder {
 
     private aligns: {
-        xs?: 'text-start' | 'text-end' | 'text-center'
+        xs?: "text-start" | "text-end" | "text-center"
     } = {};
 
     start() {
-        this.aligns.xs = 'text-start';
+        this.aligns.xs = "text-start";
         return this;
     }
 
     end() {
-        this.aligns.xs = 'text-end';
+        this.aligns.xs = "text-end";
         return this;
     }
 
     center() {
-        this.aligns.xs = 'text-center';
+        this.aligns.xs = "text-center";
         return this;
     }
 
-    private _color: ContextualClass | 'text-muted' | 'text-reset';
+    private _color: ContextualClass | "text-muted" | "text-reset";
 
     context(context: ContextualClass) {
         this._color = context;
@@ -30,31 +30,36 @@ export class TextCss implements ICssBuilder {
     }
 
     muted() {
-        this._color = 'text-muted';
+        this._color = "text-muted";
         return this;
     }
 
     resetColor() {
-        this._color = 'text-reset';
+        this._color = "text-reset";
         return this;
     }
 
-    private _truncate: 'text-truncate';
+    private _truncate: "text-truncate";
 
     truncate() {
-        this._truncate = 'text-truncate';
+        this._truncate = "text-truncate";
         return this;
     }
 
     private fontWeight: string;
 
     bold() {
-        this.fontWeight = 'fw-bold';
+        this.fontWeight = "fw-bold";
         return this;
     }
 
     bolder() {
-        this.fontWeight = 'fw-bolder';
+        this.fontWeight = "fw-bolder";
+        return this;
+    }
+
+    normalWeight() {
+        this.fontWeight = "fa-normal";
         return this;
     }
 
@@ -62,29 +67,96 @@ export class TextCss implements ICssBuilder {
 
     fontSize(size: 1 | 2 | 3 | 4 | 5) {
         this.size = size ? `fs-${size}` : null;
+        return this;
+    }
+
+    private _wrap: string;
+
+    wrap() {
+        this._wrap = "text-wrap";
+        return this;
+    }
+
+    nowrap() {
+        this._wrap = "text-nowrap";
+        return this;
     }
 
     private style: string;
 
     italicize() {
-        this.style = 'fst-italic';
+        this.style = "fst-italic";
+        return this;
+    }
+
+    private textDecoration: string;
+
+    underline() {
+        this.textDecoration = "text-decoration-underline";
+        return this;
+    }
+
+    lineThrough() {
+        this.textDecoration = "text-decoration-line-through";
+        return this;
+    }
+
+    noTextDecoration() {
+        this.textDecoration = "text-decoration-none";
+        return this;
+    }
+
+    private _textBreak: string;
+
+    textBreak() {
+        this._textBreak = "text-break";
+        return this;
+    }
+
+    private _monospace: string;
+
+    monospace() {
+        this._monospace = "font-monospace";
+        return this;
     }
 
     cssClass() {
-        let css = new CssClass();
+        const css = new CssClass();
         if (this._color) {
             if (this._color instanceof ContextualClass) {
-                css.addName(this._color.append('text'));
+                css.addName(this._color.append("text"));
             }
             else {
                 css.addName(this._color);
             }
         }
-        css.addName(this.aligns.xs);
-        css.addName(this._truncate);
-        css.addName(this.fontWeight);
-        css.addName(this.size);
-        css.addName(this.style);
+        if (this.aligns.xs) {
+            css.addName(this.aligns.xs);
+        }
+        if (this._truncate) {
+            css.addName(this._truncate);
+        }
+        if (this.fontWeight) {
+            css.addName(this.fontWeight);
+        }
+        if (this.size) {
+            css.addName(this.size);
+        }
+        if (this._wrap) {
+            css.addName(this._wrap);
+        }
+        if (this.style) {
+            css.addName(this.style);
+        }
+        if (this.textDecoration) {
+            css.addName(this.textDecoration);
+        }
+        if (this._textBreak) {
+            css.addName(this._textBreak);
+        }
+        if (this._monospace) {
+            css.addName(this._monospace);
+        }
         return css;
     }
 
